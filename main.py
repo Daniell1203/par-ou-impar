704265116097d1072315f7f865d6995511adf2a5
Carlos Daniel <danieldpaula07@gmail.com>
2026-06-09 20:40:04 -0300

    feat: adiciona verificação de par ou ímpar

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import random
 n = int(input("Digite um numero de 1 a 10: "))
 p = random.randint(0, 10)
 soma = n + p
-print(f"Soma: {soma}")
+
+resultado = "PAR" if soma % 2 == 0 else "IMPAR"
+print(f"A opção vencedora foi {resultado}")
