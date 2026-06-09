2371bd661b150ae65f192cadd4290e8b1e556ac8
Carlos Daniel <danieldpaula07@gmail.com>
2026-06-09 20:41:22 -0300

    feat: adiciona escolha do jogador e verificação de vitória/derrota

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import random
 
 n = int(input("Digite um numero de 1 a 10: "))
+esc = input("par ou impar: ").upper()
 p = random.randint(0, 10)
 soma = n + p
 
 resultado = "PAR" if soma % 2 == 0 else "IMPAR"
 print(f"A opção vencedora foi {resultado}")
+
+if esc == resultado:
+    print("Voce venceu!")
+else:
+    print("Perdeu")
